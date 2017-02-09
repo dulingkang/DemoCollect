0c9609b15734511963d21e45928efc38b1b29b8f
dulingkang@163.com <dulingkang@163.com>
2017-02-09 17:16:20 +0800

    add lotties

--- DemoCollect/DemoCollect/AppConfig.swift
+++ DemoCollect/DemoCollect/AppConfig.swift
@@ -7,34 +7,34 @@
 //
 
 import UIKit
-import Aspects
+//import Aspects
 
-struct AppConfig {
-    static func SSViewControllerConfig() {
-        let wrappedBlock:@convention(block) (AspectInfo)-> Void = { aspectInfo in
-            let instance = aspectInfo.instance() as? UIViewController
-            instance?.SSViewDidLoad()
-            }
-        let wrappedObject: AnyObject = unsafeBitCast(wrappedBlock, to: AnyObject.self)
-        do {
-            try UIViewController.aspect_hook(#selector(UIViewController.viewDidLoad), with: AspectOptions(), usingBlock: wrappedObject)
-        } catch {
-            print(error)
-        }
-    }
-}
-
-extension UIViewController {    
-    func SSViewDidLoad() {
-        self.navigationController?.navigationBar.isTranslucent = true
-        print("SSViewDidLoad")
-//        self.view.backgroundColor = UIColor.greenColor()
-        self.navigationController?.title = self.theClassName
-        if self.responds(to: #selector(getter: UIViewController.edgesForExtendedLayout)) {
-            self.edgesForExtendedLayout = UIRectEdge()
-        }
-    }
-}
+//struct AppConfig {
+//    static func SSViewControllerConfig() {
+//        let wrappedBlock:@convention(block) (AspectInfo)-> Void = { aspectInfo in
+//            let instance = aspectInfo.instance() as? UIViewController
+//            instance?.SSViewDidLoad()
+//            }
+//        let wrappedObject: AnyObject = unsafeBitCast(wrappedBlock, to: AnyObject.self)
+//        do {
+//            try UIViewController.aspect_hook(#selector(UIViewController.viewDidLoad), with: AspectOptions(), usingBlock: wrappedObject)
+//        } catch {
+//            print(error)
+//        }
+//    }
+//}
+//
+//extension UIViewController {    
+//    func SSViewDidLoad() {
+//        self.navigationController?.navigationBar.isTranslucent = true
+//        print("SSViewDidLoad")
+////        self.view.backgroundColor = UIColor.greenColor()
+//        self.navigationController?.title = self.theClassName
+//        if self.responds(to: #selector(getter: UIViewController.edgesForExtendedLayout)) {
+//            self.edgesForExtendedLayout = UIRectEdge()
+//        }
+//    }
+//}
 
 
 
