4453f0827ec69cf10b57ab6d8014a173f4ad85f7
dushuaishuai <dushuaishuai@odc.cmbchina.com>
2017-08-08 18:42:19 +0800

    update

--- DemoCollectOC/DemoCollectOC/AppList/SyncViewController.m
+++ DemoCollectOC/DemoCollectOC/AppList/SyncViewController.m
@@ -7,6 +7,12 @@
 //
 
 #import "SyncViewController.h"
+#import <objc/runtime.h>
+#import <objc/message.h>
+#import <libkern/OSAtomic.h>
+#import <pthread.h>
+
+static NSInteger const kLoopCount = 2^64;
 
 @interface SyncViewController ()
 
@@ -16,21 +22,108 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self addWhileLoop];
+    NSTimeInterval past = 1;
+    NSTimeInterval now = 0;
+    NSInteger i = 0;
+    id obj = [NSObject new];
+    dispatch_queue_t queue = dispatch_queue_create("com.shawn.barrier", DISPATCH_QUEUE_CONCURRENT);
     
-}
-
-- (void)addWhileLoop {
-    dispatch_queue_t queue = dispatch_queue_create("com.shawn.whileLoopQueue", DISPATCH_QUEUE_CONCURRENT);
-    __block BOOL complete;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        complete = YES;
-    });
-    dispatch_async(queue, ^{
+    past = CFAbsoluteTimeGetCurrent();
+    for (i = 0; i < kLoopCount; i++) {
+        dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+        dispatch_async(queue, ^{
+            dispatch_semaphore_signal(semaphore);
+        });
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"dispatch_semaphore: %f", now - past);
+    
+    past = CFAbsoluteTimeGetCurrent();
+    for (i = 0; i < kLoopCount; i++) {
+        __block BOOL complete = NO;
+        dispatch_async(queue, ^{
+            complete = YES;
+        });
         while (!complete) {
         }
-        NSLog(@"loop end.");
-    });
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"whileLoop: %f", now - past);
+    
+    past = CFAbsoluteTimeGetCurrent();
+    for(i = 0;i < kLoopCount; i++) {
+        dispatch_barrier_async(queue, ^{});
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"dispatch_barrier_async: %f", now - past);
+    
+    past = CFAbsoluteTimeGetCurrent();
+    for (i = 0; i < kLoopCount; i++) {
+        @synchronized (obj) {
+        }
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"@synchronized: %f", now - past);
+    
+    NSLock *lock = [NSLock new];
+    past = CFAbsoluteTimeGetCurrent();
+    for (i = 0; i < kLoopCount; i++) {
+        [lock lock];
+        [lock unlock];
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"NSLock: %f", now - past);
+    
+    typedef void (*func)(id, SEL);
+    SEL lockSEL = @selector(lock);
+    SEL unlockSEL = @selector(unlock);
+    func lockFunc = (void (*)(id, SEL))[lock methodForSelector : lockSEL];
+    func unlockFunc = (void (*)(id, SEL))[lock methodForSelector : unlockSEL];
+    past = CFAbsoluteTimeGetCurrent();
+    for (i = 0; i < kLoopCount; i++) {
+        lockFunc(lock, lockSEL);
+        unlockFunc(lock, lockSEL);
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"NSLock+IMP: %f", now - past);
+    
+    NSConditionLock *conditionLock = [NSConditionLock new];
+    past = CFAbsoluteTimeGetCurrent();
+    for (i = 0; i < kLoopCount; i++) {
+        [conditionLock lock];
+        [conditionLock unlock];
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"NSConditionLock: %f", now - past);
+    
+    NSRecursiveLock *recursiveLock = [NSRecursiveLock new];
+    past = CFAbsoluteTimeGetCurrent();
+    for (i = 0; i < kLoopCount; i++) {
+        [recursiveLock lock];
+        [recursiveLock unlock];
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"NSRecursiveLock: %f", now - past);
+    
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    past = CFAbsoluteTimeGetCurrent();
+    for(i = 0;i < kLoopCount; i++) {
+        pthread_mutex_lock(&mutex);
+        pthread_mutex_unlock(&mutex);
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"pthread_mutex: %f", now - past);
+    
+    OSSpinLock spinlock = OS_SPINLOCK_INIT;
+    past = CFAbsoluteTimeGetCurrent();
+    for(i = 0;i < kLoopCount; i++) {
+        OSSpinLockLock(&spinlock);
+        OSSpinLockUnlock(&spinlock);
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"OSSpinLock: %f", now - past);
 }
+
 
 @end
